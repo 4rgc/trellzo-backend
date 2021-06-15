fb05874f9f253adb118b13ca170ed859d990e520
gemumeka <justkrut514@gmail.com>
2021-06-15 22:52:02 +0300

    feat: add temporary routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
-import bookController from './controllers/comment';
 import mongoose from 'mongoose';
+import userController from './controllers/user';
 
 const app = express();
 
-mongoose.connect('mongodb://mongodb:27017');
+app.use(express.json());
+
+mongoose.connect('mongodb://mongodb:27017/trellzo').catch((err) => {
+	console.error(err.message);
+});
 
 app.get('/', (req, res) => {
 	res.send('Well done!');
 });
 
-app.get('/comments', bookController.getAllComments);
+app.get('/users', userController.getAllUsers);
+app.get('/users/addNewComment', userController.addNewComment);
 
 app.listen(8080, () => {
 	console.log('Listening on port 8080...');
