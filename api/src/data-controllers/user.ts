eef3f74acab62b8e24e6c073bcc3c153487cc002
gemumeka <justkrut514@gmail.com>
2021-06-17 21:42:55 +0300

    refactor: remove express logic from data access methods

--- api/src/data-controllers/user.ts
+++ api/src/data-controllers/user.ts
@@ -1,91 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import User from '../models/user';
 
-const getAllUsers = (req: Request, res: Response, next: NextFunction) => {
-	User.find()
-		.exec()
-		.then((result) =>
-			res.json({
-				users: result,
-				count: result.length,
-			})
-		)
-		.catch((err) =>
-			res.status(500).json({
-				message: err.message,
-				err,
-			})
-		);
-};
+const getAllUsers = () => User.find().lean().exec();
 
-const getUserByEmail = (req: Request, res: Response, next: NextFunction) => {
-	if (typeof req.query.email !== 'string') {
-		return res.status(400).json({
-			message:
-				'email query parameter was of type ' +
-				typeof req.query.email +
-				'; expected: string',
-		});
-	}
-	User.findOne({ email: req.query.email }, { boards: 0 })
-		.exec()
-		.then((user) => {
-			if (!user)
-				return res.status(404).json({
-					message: 'User with id=' + req.params.userId + ' not found',
-				});
-			res.json({
-				user,
-			});
-		})
-		.catch((err) => {
-			res.status(500).json({
-				message: err.message,
-				err,
-			});
-		});
-};
+const getUserByEmail = (req: Request) =>
+	User.findOne({ email: req.query.email as string }, { boards: 0 })
+		.lean()
+		.exec();
 
-const getUserBoards = (req: Request, res: Response, next: NextFunction) => {
+const getUserBoards = (req: Request) =>
 	User.findOne(
 		{ _id: req.query.userId },
 		{ _id: 0, name: 0, email: 0, boards: { lists: 0, listsOrder: 0 } }
 	)
 		.lean()
-		.exec()
-		.then((user) => {
-			res.json({ ...user });
-		})
-		.catch((err) => {
-			res.status(500).json({
-				message: err.message,
-				err,
-			});
-		});
-};
+		.exec();
 
-const createNewUser = (req: Request, res: Response, next: NextFunction) => {
+const createNewUser = (req: Request) =>
 	User.create({
 		name: req.body.name,
 		email: req.body.email,
-	})
-		.then(() => User.findOne({ email: req.body.email }).exec())
-		.then((user) => {
-			res.status(201).json({
-				user,
-			});
-		})
-		.catch((err) => {
-			let status = 500;
-			if (err.name === 'ValidationError') status = 400;
-			res.status(status).json({
-				message: err.message,
-				err,
-			});
-		});
-};
-
-const updateUser = (req: Request, res: Response, next: NextFunction) => {
+	}).then(() => User.findOne({ email: req.body.email }).exec());
+/* 
+const updateUserHandler = (req: Request, res: Response, next: NextFunction) => {
 	console.log(req.body.email);
 	User.findByIdAndUpdate(
 		req.params.userId,
@@ -108,7 +45,17 @@ const updateUser = (req: Request, res: Response, next: NextFunction) => {
 				err,
 			});
 		});
-};
+}; */
+
+const updateUser = (req: Request) =>
+	User.findByIdAndUpdate(
+		req.params.userId,
+		{
+			email: req.body.email,
+			name: req.body.name,
+		},
+		{ new: true, omitUndefined: true }
+	);
 
 export default {
 	getAllUsers,
