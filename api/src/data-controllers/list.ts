cac472fe03cc020daa1a6212b71ba64baa8ee140
gemumeka <justkrut514@gmail.com>
2021-06-23 18:27:11 +0300

    feat: implement list data controller

---
+++ api/src/data-controllers/list.ts
@@ -0,0 +1,58 @@
+import User from '../models/user';
+
+const createList = (userId: string, boardId: string, listName: string) =>
+	User.findByIdAndUpdate(
+		userId,
+		{
+			$push: { 'boards.$[boardField].lists': { name: listName } },
+		},
+		{ arrayFilters: [{ 'boardField._id': boardId }], new: true }
+	)
+		.exec()
+		.then((user) => user?.boards[0].lists[0]);
+
+const updateList = (
+	userId: string,
+	boardId: string,
+	listId: string,
+	name: string,
+	description: string
+) =>
+	User.findByIdAndUpdate(
+		userId,
+		{
+			'boards.$[boardField].lists.$[listField].name': name,
+			'boards.$[boardField].lists.$[listField].description': description,
+		},
+		{
+			arrayFilters: [
+				{ 'boardField._id': boardId, 'listField._id': listId },
+			],
+			omitUndefined: true,
+			new: true,
+		}
+	)
+		.exec()
+		.then((user) => user?.boards[0].lists[0]);
+
+const deleteList = (userId: string, boardId: string, listId: string) =>
+	User.findByIdAndUpdate(
+		userId,
+		{ 'boards.$[boardField].lists.$[listField]': null },
+		{
+			arrayFilters: [
+				{ 'boardField._id': boardId },
+				{ 'listField._id': listId },
+			],
+		}
+	)
+		.exec()
+		.then((user) => user?.boards[0].lists[0]);
+
+const listDataController = {
+	createList,
+	updateList,
+	deleteList,
+};
+
+export default listDataController;
