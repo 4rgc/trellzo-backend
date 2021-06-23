9de82047e0bcc99542567e85910f714c3d77c8e8
gemumeka <justkrut514@gmail.com>
2021-06-23 19:46:45 +0300

    feat: implement list controller methods

---
+++ api/src/controllers/list.ts
@@ -0,0 +1,71 @@
+import listDataController from '../data-controllers/list';
+import { Request, Response, NextFunction } from 'express';
+
+const addList = async (req: Request, res: Response, next: NextFunction) => {
+	const { userId } = res.locals.auth;
+	const { boardId } = req.params;
+	const { name } = req.body;
+
+	if (!userId) return res.status(400).json({ message: 'userId was null' });
+	if (!boardId) return res.status(400).json({ message: 'boardId was null' });
+	if (!name) return res.status(400).json({ message: 'List name was null' });
+
+	const newList = await listDataController
+		.createList(userId, boardId, name)
+		.catch(next);
+
+	res.status(201).json({
+		list: newList,
+	});
+};
+
+const updateList = async (req: Request, res: Response, next: NextFunction) => {
+	const { userId } = res.locals.auth;
+	const { boardId, listId } = req.params;
+	const { name, description } = req.body;
+
+	if (!userId) return res.status(400).json({ message: 'userId was null' });
+	if (!boardId) return res.status(400).json({ message: 'boardId was null' });
+	if (!listId) return res.status(400).json({ message: 'listId was null' });
+
+	if (!name && !description) return res.send();
+
+	const updatedList = await listDataController.updateList(
+		userId,
+		boardId,
+		listId,
+		name,
+		description
+	);
+
+	res.json({
+		list: updatedList,
+	});
+};
+
+const removeList = async (req: Request, res: Response, next: NextFunction) => {
+	const { userId } = res.locals.auth;
+	const { boardId, listId } = req.params;
+
+	if (!userId) return res.status(400).json({ message: 'userId was null' });
+	if (!boardId) return res.status(400).json({ message: 'boardId was null' });
+	if (!listId) return res.status(400).json({ message: 'listId was null' });
+
+	const deletedList = await listDataController.deleteList(
+		userId,
+		boardId,
+		listId
+	);
+
+	res.json({
+		list: deletedList,
+	});
+};
+
+const listController = {
+	addList,
+	updateList,
+	removeList,
+};
+
+export default listController;
