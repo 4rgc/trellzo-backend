5ae4e848b494c65bbb0e8ae56e058e6fd36f7858
gemumeka <justkrut514@gmail.com>
2021-07-09 13:49:58 -0400

    feat: implement partial note schema

---
+++ api/src/schemas/partialNote.ts
@@ -0,0 +1,11 @@
+import IPartialNote from '../interfaces/partialNote';
+import TagSchema from './tag';
+import { Schema } from 'mongoose';
+
+const PartialNoteSchema = new Schema<IPartialNote>({
+	name: { type: String, required: true },
+	description: { type: String, default: '' },
+	startDate: { type: Schema.Types.Date, default: null },
+	dueDate: { type: Schema.Types.Date, default: null },
+	tags: { type: [TagSchema], default: [] },
+});
