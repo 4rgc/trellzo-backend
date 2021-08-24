e6e69333fa4ce64d3b5bbeb666c76819b4c00eaf
gemumeka <justkrut514@gmail.com>
2021-08-24 10:35:10 -0400

    feat: add an api-docs route for swagger documentation

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { queryParser } from 'express-query-parser';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import boardDataRouter from './routes/board-data';
 import usersDataRouter from './routes/users-data';
 import userDataRouter from './routes/user-data';
@@ -17,6 +19,45 @@ import listRouter from './routes/list';
 import noteRouter from './routes/note';
 import checklistRouter from './routes/checklist';
 import commentRouter from './routes/comments';
+
+const options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Trellzo API',
+			version: '1.0.0',
+		},
+		components: {
+			schemas: {
+				ResultMessage: {
+					type: 'object',
+					properties: {
+						message: {
+							type: 'string',
+							description:
+								"The description of the request's outcome.",
+						},
+					},
+				},
+			},
+			securitySchemes: {
+				jwtAuth: {
+					type: 'apiKey',
+					in: 'cookie',
+					name: 'auth',
+				},
+				jwtRef: {
+					type: 'apiKey',
+					in: 'cookie',
+					name: 'reft',
+				},
+			},
+		},
+	},
+	apis: ['./src/routes/*.ts', './src/index.ts'],
+};
+
+const openapiSpecification = swaggerJsdoc(options);
 
 const app = express();
 
@@ -34,6 +75,8 @@ mongoose
 		process.exit();
 	});
 mongoose.set('runValidators', true);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.use('/data/user', userDataRouter);
 app.use('/data/users', usersDataRouter);
